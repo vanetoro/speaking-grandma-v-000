c6629c9bd368b136f570c57c942176c384df7de3
Vanessa Toro <vanetoro84@gmail.com>
2018-03-21 01:35:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(something)
-  if is_upper?(something)
+  if something.upcase
     puts 'NO, NOT SINCE 1938'
   elsif something == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
