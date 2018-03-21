f2ad266841ab3dc19221b8865e757ba559ca167f
Vanessa Toro <vanetoro84@gmail.com>
2018-03-21 01:40:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(something)
   elsif something == something.upcase
     return 'NO, NOT SINCE 1938'
   else
-    return "HUH?! SPEAK UP SONNY"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
