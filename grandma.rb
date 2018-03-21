0dc7b527da6cf9d782a10293f1593b4cd4f12209
Vanessa Toro <vanetoro84@gmail.com>
2018-03-21 01:40:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(something)
-  if something == something.upcase
-    return 'NO, NOT SINCE 1938'
-  elsif something == 'I LOVE YOU GRANDMA!'
+  if something == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
+  elsif something == something.upcase
+    return 'NO, NOT SINCE 1938'
   else
     return "HUH?! SPEAK UP SONNY"
   end
